7dd448f0c2f7c577d57ca1992b5289dc5cc04fd5
Sameer Nadaf <nsameernadaf@gmail.com>
2026-03-31 12:10:11 +0530

    Initial Commit

---
+++ Master-SSO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Master-SSO
+//
+//  Created by CIPL User01 on 31/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Master-SSO/Master_SSOApp.swift
@@ -0,0 +1,17 @@
+//
+//  Master_SSOApp.swift
+//  Master-SSO
+//
+//  Created by CIPL User01 on 31/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Master_SSOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
